91544fc889eb6e9c22c9f7118d70237d6ffe40c5
InKyu <484342@gmail.com>
2021-04-01 17:59:45 +0900

    Practice to use KAKAO API [Search]

---
+++ ai/src/main/java/my/cik/ai/test4/kakaosearch/ConTestKakaoSearch.java
@@ -0,0 +1,58 @@
+package my.cik.ai.test4.kakaosearch;
+
+//
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConTestKakaoSearch {
+	public static void main(String[] args) {
+		try {
+
+			String text = URLEncoder.encode("이효리", "UTF-8");
+			URL url = new URL("https://dapi.kakao.com/v2/search/web?query="+text);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Authorization", "KakaoAK f240f525b7efb9f1b64e9f0ee8023742");
+		    
+			
+//			con.setDoOutput(true);
+//			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+//			
+//			wr.writeBytes(text);// 전송
+//			wr.flush();
+//			wr.close();
+
+			int responseCode = con.getResponseCode();
+			System.out.println(responseCode);
+			BufferedReader br;
+			if (responseCode == 200) { // 정상 호출
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+				while ((inputLine = br.readLine()) != null) {
+					response.append(inputLine);
+				}
+				br.close();
+				System.out.println(response.toString());
+			} else { // 오류 발생
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+				while ((inputLine = br.readLine()) != null) {
+					response.append(inputLine);
+				}
+				br.close();
+				System.out.println(response.toString());
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
