4d021a654b542a6bf7c6f773479cea0fcab1a6e0
InKyu <484342@gmail.com>
2021-04-06 10:59:26 +0900

    Modify HTML file to use fileUpload function [another way]

--- SpringBoot01_Mybatis_Cafe2/src/main/java/com/mulcam/ai/web/controller/UploadController.java
+++ SpringBoot01_Mybatis_Cafe2/src/main/java/com/mulcam/ai/web/controller/UploadController.java
@@ -12,16 +12,19 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadController {
 	
 	@PostMapping("/upload")
-	public void upload(@RequestParam("file") MultipartFile file) {
+	public String upload(@RequestParam("file") MultipartFile file) {
 		System.out.println("파일인" + file + "이 들어옴");
 		try {
 			file.transferTo(new File("D:\\GITHUB\\STS_workspace\\SpringBoot01_Mybatis_Cafe2\\src\\main\\resources\\static\\img\\web_cam\\"+file.getOriginalFilename()));
+			return "upload 성공";
 		} catch (IllegalStateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return "upload 실패";
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return "upload 실패";
 		}
 	}
 }
